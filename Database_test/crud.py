a42f4799204396fab2a76915287c4066cae699b1
Abhishek <dummy1cx@gmail.com>
2025-09-25 15:33:37 +0100

    updated code

--- Database_test/crud.py
+++ Database_test/crud.py
@@ -30,7 +30,7 @@ def delete_employee(db: Session, emp_id : int):
     db_employee = db.query(models.Employee).filter(models.Employee.id == emp_id).first()
     if db_employee :
         db.delete(db_employee)
-        db.comit()
+        db.commit()
 
     return db_employee
 
