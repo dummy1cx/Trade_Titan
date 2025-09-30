bdb5cf9b9cca7819ffd2c85676770140b876d99c
Abhishek <dummy1cx@gmail.com>
2025-09-30 10:21:20 +0100

    test

--- inte-test/test/test_main.py
+++ inte-test/test/test_main.py
@@ -18,4 +18,15 @@ def test_eligibility_pass():
     assert response.json() == {'eligible' : True}
 
 
+def test_eligibility_fail():
+    payload = {
+        'income': 30000,
+        'age': 18,
+        'employment_status': 'unemployed'
+
+    }
+    response = client.post('/loan-eligibility', json = payload)
+    assert response.status_code == 200
+    assert response.json() == {'eligible': False}
+
 
